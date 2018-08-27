8373174aaf88c344ca11238056b70be81b9da53b
FerrisChan <ferris.chan@foxmail.com>
2018-08-27 16:42:01 +0800

    修改文件名

--- app.py
+++ app.py
@@ -42,5 +42,5 @@ if __name__ == '__main__':
         host='0.0.0.0',
         port=2000,
     )
-    logging.basicConfig(filename='error.log', level=logging.DEBUG)
+    logging.basicConfig(filename='flask.log.txt', level=logging.DEBUG)
     app.run(**config)
